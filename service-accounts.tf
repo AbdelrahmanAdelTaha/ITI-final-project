af532c88f39e3e5485e428304280b5bf4ca49adb
Abdelrahman <abdelrahman19.adel@gmail.com>
2022-11-04 16:48:22 +0200

    last configurations

--- service-accounts.tf
+++ service-accounts.tf
@@ -4,20 +4,25 @@ resource "google_service_account" "vm-service-account" {
   account_id = "vm-service-account"
 }
 
-resource "google_project_iam_binding" "vm-service-account-iam" {
-  project = "abdelrahmanadel-project2"
-  role    = "roles/container.admin"
-  members = [
-    "serviceAccount:${google_service_account.vm-service-account.email}",
-  ]
-}
-
-
 //service account form cluster nodes 
 
 resource "google_service_account" "nodes-service-account" {
   account_id = "node-service-account"
 }
+
+resource "google_project_iam_binding" "vm-service-account-iam" {
+  project = "abdelrahmanadel-project2"
+  role    = "roles/container.admin"
+  members = [
+    "serviceAccount:${google_service_account.vm-service-account.email}",
+    "serviceAccount:${google_service_account.nodes-service-account.email}",
+  ]
+}
+
+
+
+
+
 
 resource "google_project_iam_binding" "nodes-service-account-iam" {
   project = "abdelrahmanadel-project2"
